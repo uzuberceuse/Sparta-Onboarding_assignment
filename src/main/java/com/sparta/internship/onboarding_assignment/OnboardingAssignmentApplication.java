ea0e1907386498a937ac25ba1deab9a078745d09
uzuberceuse <cerulesky@naver.com>
2024-11-10 12:08:25 +0900

    Initial commit

---
+++ src/main/java/com/sparta/internship/onboarding_assignment/OnboardingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.internship.onboarding_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingAssignmentApplication.class, args);
+    }
+
+}
